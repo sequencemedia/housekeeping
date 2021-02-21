cfe8cbdf4297dcbf32973f3e9fac3bb0dcc56d6e
Jonathan Perry <jonathanperry@sequencemedia.net>
2021-02-21 21:00:12 +0000

    Husky

--- src/package.js
+++ src/package.js
@@ -1,13 +1,21 @@
 import debug from 'debug'
 
 import {
-  resolve
+  resolve,
+  dirname,
+  join
 } from 'path'
 
 import {
   readFile,
   writeFile
 } from 'fs/promises'
+
+import ensureDir from 'ensureDir'
+
+import {
+  exec
+} from 'child_process'
 
 import getPackages from './common/get-packages'
 
@@ -17,7 +25,13 @@ log('`housekeeping:package` is awake')
 
 const transform = (v) => resolve(v) // constrain to one arg
 
-async function execute (p, AUTHOR) {
+const ensure = async (p) => (
+  new Promise((resolve, reject) => {
+    ensureDir(p, 755, (e) => !e ? resolve() : reject(e))
+  })
+)
+
+async function execute (p, AUTHOR, REGEXP, d) {
   log('execute')
 
   let s = await readFile(p, 'utf8')
@@ -71,8 +85,31 @@ async function execute (p, AUTHOR) {
     ...(devDependencies ? { devDependencies } : {}),
     ...(peerDependencies ? { peerDependencies } : {}),
     ...rest,
-    ...(_moduleAliases ? { _moduleAliases } : {}),
-    ...(husky ? { husky } : {})
+    ...(_moduleAliases ? { _moduleAliases } : {})
+  }
+
+  if (husky) {
+    const {
+      hooks = {}
+    } = husky
+
+    const h = resolve(join(dirname(p), '.husky'))
+
+    await ensure(h)
+
+    const a = Array
+      .from(Object.entries(hooks))
+      .map(([key, value]) => (
+        new Promise((resolve) => {
+          exec(`npx husky add "${h}/${key}" "${value}"`, (e) => {
+            if (e) log(`Error at "${h}/${key}"`)
+
+            resolve()
+          })
+        }))
+      )
+
+    await Promise.all(a)
   }
 
   s = JSON.stringify(o, null, 2).concat('\n')
