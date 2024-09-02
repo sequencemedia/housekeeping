b7f62e00865f9814c06df49670bd99ccbfb5cea2
Jonathan Perry <jonathanperry@sequencemedia.net>
2024-09-02 01:11:04 +0100

    `toPackages`

--- src/package.mjs
+++ src/package.mjs
@@ -9,7 +9,7 @@ import getFilePaths from './common/get-file-paths.mjs'
 import genFilePath from './common/gen-file-path.mjs'
 import fromFile from './common/from-file.mjs'
 import toFile from './common/to-file.mjs'
-import getPackages from './common/get-packages.mjs'
+import toPackages from './common/to-packages.mjs'
 import handleError from './common/handle-error.mjs'
 import isString from './common/is-string.mjs'
 
@@ -129,7 +129,7 @@ export default async function handleDirectory (directory, author, regExp) {
   try {
     info(d)
 
-    const a = await getPackages(d)
+    const a = await getFilePaths(toPackages(d))
     for (const filePath of genFilePath(a)) await handlePackageDirectory(dirname(filePath), author, regExp)
   } catch (e) {
     handleError(e)
